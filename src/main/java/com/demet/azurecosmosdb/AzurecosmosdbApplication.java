71db07a33edc9448273ae38f73d76f0b3271c4a8
demetyy <demetsenn@yahoo.com.tr>
2019-04-21 00:27:05 +0300

    initial sprint boot

---
+++ src/main/java/com/demet/azurecosmosdb/AzurecosmosdbApplication.java
@@ -0,0 +1,13 @@
+package com.demet.azurecosmosdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurecosmosdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurecosmosdbApplication.class, args);
+	}
+
+}
